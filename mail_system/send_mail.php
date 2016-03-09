792e873286572d73489d4500bed51d1222df4505
Panayiotis Pavlides <ppavli02@cs.ucy.ac.cy>
2016-03-09 20:32:37 +0200

    ignore user abort

--- mail_system/send_mail.php
+++ mail_system/send_mail.php
@@ -1,5 +1,9 @@
 <?php
+#Ignore the closing window
+ignore_user_abort(true);
+set_time_limit(0);
 
+#Execute the algorithm
 exec('./dokimi 2>&1', $output);
 $message="";
 
@@ -7,14 +11,12 @@ foreach ($output as &$line) {
     $message .= "$line<br />";
 }
 
-
+#Send Mail
 require_once('class.phpmailer.php');
 require_once('class.smtp.php');
-
                              
-$mail = new PHPMailer();                                     // Set mailer to use SMTP\
+$mail = new PHPMailer();
 $mail->IsSMTP();
-$mail->SMTPDebug = 3;
 $mail->SMTPAuth = true;
 $mail->Host = "smtp.gmail.com";
 $mail->Port = 587;
@@ -22,13 +24,13 @@ $mail->Username = "andreasfrangou3@gmail.com";
 $mail->Password = "katiaapanotou";
 
 $mail->SetFrom('andreasfrangou3@gmail.com', 'Web App');
-$mail->Subject = "A Transactional Email From Web App";
+$mail->Subject = "Your PSSP Results";
 $mail->MsgHTML($message);
 $mail->AddAddress("panayiotis.pavlides@gmail.com", "Andreakis");
 
 if($mail->Send()) {
-    echo "Message sent!";
+    //echo "Message sent!";
 } else {
-    echo "Mailer Error: " . $mail->ErrorInfo;
+    //echo "Mailer Error: " . $mail->ErrorInfo;
 }
 ?>
