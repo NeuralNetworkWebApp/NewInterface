eafc68268c3ca72273dcff67caa41dcc12be3e07
epapak02 <epapak02@cs.ucy.ac.cy>
2016-03-09 20:37:33 +0200

    correctDB

--- setSelectionLists.php
+++ setSelectionLists.php
@@ -10,23 +10,17 @@ $Model = $conn->prepare("SELECT title FROM model");
 $Model->execute();
 
 $ResultModel = $Model->fetchAll(PDO::FETCH_ASSOC);
-foreach($ResultModel as $row) {
-    print $row['title'];
-}
 
-$TrainAlgorithm = $conn->prepare("SELECT title FROM trainalgorithm");
+
+$TrainAlgorithm = $conn->prepare("SELECT title FROM train_algorithm");
 $TrainAlgorithm->execute();
 
 $ResultTrainAlgorithm = $TrainAlgorithm->fetchAll(PDO::FETCH_ASSOC);
-foreach($ResultTrainAlgorithm as $row) {
-    print $row['title'];
-}
+
 
 $TestSet = $conn->prepare("SELECT title FROM testset");
 $TestSet->execute();
 $ResultTestSet = $TestSet->fetchAll(PDO::FETCH_ASSOC);
-foreach($ResultTestSet as $row) {
-    print $row['title'];
-}
+
 
 ?>
