0788c6b065d040ccd7a63332ab88cf61a9a62076
epapak02 <epapak02@cs.ucy.ac.cy>
2016-05-05 11:53:15 +0300

    Interface Changes
    Required input, change css

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 include "setSelectionLists.php";
 //let's start the session
-session_start();
 //finally, let's store our posted values in the session variables
-$_SESSION['name'] = "";
-$_SESSION['isLoggedIn']=false;
-$_SESSION['email'] = "";
+
 
 ?>
 
@@ -20,24 +17,29 @@ $_SESSION['email'] = "";
 		<title>Landed by HTML5 UP</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
 		<!--[if lte IE 8]><script src="js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/main.css" />
 		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
-        <script src="js/landing_page_handlers.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script
+
+
 	</head>
 	<body class="landing">
-		<div id="page-wrapper">
 
+		<div id="page-wrapper">
 			<!-- Header -->
 				<header id="header">
 					<h1 id="logo"><a href="index.php">Landed</a></h1>
 					<nav id="nav">
 						<ul>
 							<li><a href="index.php">Home</a></li>
-							<li><a href="#">Help</a></li>
+							<li><a href="#four">Help</a></li>
 							<li><a href="#"></a></li>
-							<li><a href="logIn_SignUp.php" class="button special">LogIn/SignUp</a></li>
+							<li><a href="#two" class="button special">Retrieve Results</a></li>
 						</ul>
 					</nav>
 				</header>
@@ -45,155 +47,101 @@ $_SESSION['email'] = "";
 			<!-- Banner -->
 				<section id="banner">
 					<div class="content">
-						<header>
-							<h3>Predict the secondary structure of proteins</h3>
-							<p>Collect results from trained Neural <br />
-								 Networks or create your own!</p>
-						</header>
-						<!--<span class="image"><img src="images/pic01.jpg" alt="" /></span>-->
-					</div>
-					<a href="#one" class="goto-next scrolly">Next</a>
-				</section>
+						<form id="myform" >
+							<div class="row uniform 50%">
+								<header class="major 12u$">
+									<h2>Run a trained Neural Network</h2>
+								</header>
+								<div class="container center_label">
+									<label for="protein" >Enter the amino acids of proteins:</label>
+								</div>
+								<div class="container center_input">
+									<div id="div_protein" class="form-group ">
+                                        <label class="control-label" style="display: none" id="protein_required" for="protein">Required</label>
+                                        <input type="text" class="form-control" aria-describedby="protein_ErrorStatus" name="protein" id="protein" value="" placeholder="protein"  />
+                                        <span class="glyphicon glyphicon-remove form-control-feedback" style="display: none" id="protein_xMark" aria-hidden="true"></span>
+                                        <span id="protein_ErrorStatus"  style="display: none" class="sr-only" >(error)</span>
 
-			<!--New Network -->
-				<?php if ($_SESSION["isLoggedIn"]) {?>
-					<section id="two" class="wrapper style1 special fade-up">
-						<div class="container">
-							<header>
-								<h2>Create your own Network </h2>
-							</header>
-							<ul class="actions">
-								<li><a href="logIn_SignUp.php" class="button">Learn More</a></li>
-							</ul>
-						</div>
-						<a href="#three" style="display: block;" class="goto-next scrolly">Next</a>
-					</section>
-				<?php }else{?>
-					<section id="one" class="wrapper style1 special fade-up">
-						<div class="container">
-							<header>
-								<h2>Create your own Network </h2>
-								<p>You have to first Log In or Sign Up</p>
-							</header>
-							<ul class="actions">
-								<li><a href="logIn_SignUp.php" class="button">LogIn/SignUp</a></li>
-							</ul>
-						</div>
-						<a href="#two" style="display: block;" class="goto-next scrolly">Next</a>
-					</section>
-				<?php }?>
-
-			<!-- Trained Network -->
-					<section id="three" class="wrapper style2 special fade-up">
-						<div class="container">
-							<form action="session.php" method="get">
-								<div class="row uniform 50%">
-									<header class="major 12u$">
-										<h2>Run a trained Neural Network</h2>
-										<p>Collect the result set</p>
-									</header>
-									<div class="6u 12u$(xsmall)">
-										<input type="text" name="name" id="name" value="" placeholder="Name" />
-									</div>
-									<div class="6u$ 12u$(xsmall)">
-										<input type="email" name="email" id="email" value="" placeholder="Email" />
-									</div>
-									<div class="12u$">
-										<div class="select-wrapper">
-											<select name="selModel" id="selModel">
-												<option value="1" type="text">- Model -</option>
-												<?php
-												$i=2;
-												foreach($ResultModel as $row){
-													print '<option value="'.$i++.'" type="text">'.$row['title'].'</option>';
-												}
-												?>
-											</select>
-										</div>
-									</div>
-									<div class="12u$">
-										<div class="select-wrapper">
-											<select name="selTalgorithm" id="selTalgorithm">
-												<option value="1" type="text">- Training Algorithm -</option>
-												<?php
-												$i=2;
-												foreach($ResultTrainAlgorithm as $row) {
-													print '<option value="'.$i++.' type="text"">'.$row['title'].'</option>';
-												}
-												?>
-											</select>
-										</div>
-									</div>
-									<div class="12u$">
-										<div class="select-wrapper">
-											<select name="selTest" id="selTest">
-												<option value="1" type="text">- Test Set -</option>
-												<?php
-												$i=2;
-												foreach($ResultTestSet as $row) {
-													print '<option value="'.$i++.' type="text"">'.$row['title'].'</option>';
-												}
-												?>
-											</select>
-										</div>
-									</div>
-									<div class="12u$">
-										<ul class="actions">
-											<li><input type="submit" value="Send Message" class="special" /></li>
-											<li><input type="reset" value="Reset" /></li>
-										</ul>
 									</div>
 								</div>
-							</form>
-						</div>
-					</section>
+								<div class="container center_label">
+									<label for="Network" >Choose Network:</label>
+								</div>
+								<div class="container center_input">
+									<div class="form-group">
+										<div class="select-wrapper">
+											<select name="category" id="category">
+                                                <?php $i=0;while($i<count($ResultModel)){?>
+												<option value=""><?php echo $ResultModel[$i]['title'];?></option>
+                                                <?php $i++;}?>
+											</select>
+										</div>
+									</div>
+								</div>
 
-            <!--Run Trained Algorithm-->
-                <section id="two" class="wrapper style1 special fade-up">
-                    <div class="container">
-                        <header>
-                            <h2>Run our trained algorithm.</h2>
-                        </header>
-                        <div class="row uniform 50%">
-                            <div class="6u 12u$(xsmall)">
-                                <input type="text" name="name" id="name_trained" value="" placeholder="Name" />
-                            </div>
-                            <div class="6u$ 12u$(xsmall)">
-                                <input type="email" name="email" id="email_trained" value="" placeholder="Email" />
-                            </div>
-                            <div class="12u$">
-                                <ul class="actions">
-                                    <li><a href="#"  onClick="runAlgorithm()" class="button">Run</a></li>
-<!--                                    <li><a href="logIn_SignUp.php" class="button">Run</a></li>-->
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+								<div class="12u$">
+									<ul class="actions">
+										<li><input type="reset" id="reset1" value="Reset"/></li>
+										<li><a  href="#banner" id="results" class="button ">Get Results</a></li>
 
-                <section id="two" class="wrapper style1 special fade-up">
-                    <div class="container">
-                        <header>
-                            <h2>Retrieve data.</h2>
-                        </header>
-                        <div class="row uniform 50%">
-                            <div class="12u 12u$(xsmall)">
-                                <input type="text" name="name" id="md5_password" value="" placeholder="MD5 Hashcode" />
-                            </div>
+									</ul>
+								</div>
 
-                            <div class="12u$">
-                                <ul class="actions">
-                                    <li><a href="#"  onClick="retrieveResults()" class="button">Run</a></li>
-                                    <!--                                    <li><a href="logIn_SignUp.php" class="button">Run</a></li>-->
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+							</div>
+						</form>
+					</div>
+					<a href="#two" class="goto-next scrolly">Next</a>
+				</section>
 
+			<!--Retrieve Data-->
+			<section id="two" class="spotlight style1 bottom">
+				<div class="content">
+					<div class="container">
+						<form >
+							<div class="row uniform 50%">
+								<header class="major 12u$">
+									<h2>Retrive Results from Network</h2>
+								</header>
+								<div class="container center_label">
+									<label for="protein" >Place your code to get your results:</label>
+								</div>
+								<div class="container center_input">
+										<div id="div_placeCode" class="form-group">
+											<label class="control-label" style="display: none" id="placeCode_required" for="getResults">Required</label>
+											<input type="text" name="getResults" aria-describedby="placeCode_ErrorStatus"  id="getResults" value="" placeholder="code" />
+											<span class="glyphicon glyphicon-remove form-control-feedback" style="display: none" id="placeCode_xMark" aria-hidden="true"></span>
+											<span id="placeCode_ErrorStatus"  style="display: none" class="sr-only" >(error)</span>
+										</div>
+								</div>
+								<div class="12u$">
+									<ul class="actions">
+										<li><input type="reset" id="reset2" value="Reset"/></li>
+										<li><a  href="#two" id="submit" class="button ">Submit</a></li>
+									</ul>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+				<a href="#four" class="goto-next scrolly">Next</a>
+			</section>
 
+			<!-- Information -->
+			<section id="four" class="wrapper style2 special fade-up">
+				<div class="container">
+					<header class="major">
+						<h2>What this project is all about</h2>
+						<p>Iaculis ac volutpat vis non enim gravida nisi faucibus posuere arcu consequat</p>
+					</header>
+					<div class="box alt">
+						<p>Proteins are ...</p>
+                        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#modal_contact_us">Conduct Us</button>
+					</div>
+				</div>
+			</section>
 			<!-- Footer -->
 				<footer id="footer">
+
 					<ul class="icons">
 						<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
 						<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
@@ -206,11 +154,129 @@ $_SESSION['email'] = "";
 						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
 					</ul>
 				</footer>
-
 		</div>
 
+        <!-- Modal Email/GenerateCode -->
+        <div class="modal fade" id="modal_email_generateCode" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content" id="modalA_content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Modal Header</h4>
+                    </div>
+                    <div class="modal-body" id="modalA_body">
+                        <p>To get the results we can eather send them to your email or you can take a code and...</p>
+                        <div class="row">
+                            <label for="email" id="emailLabel" >Email:</label>
+                            <input class="form-control" type="email"  name="email" id="email" value="" placeholder="Email" required />
+                        </div>
+                        <div class="row">
+                            <label>Or</label>
+                        </div>
+                        <div class="6u 12u$(medium)" id="generateCode" >
+                            <input type="checkbox" id="code" name="code" >
+                            <label for="code">Generate Code</label>
+                        </div>
+<!--                        <div class="row" id="div_GenerateCode">-->
+<!--<!--                            <label for="generatedCode" id="emailLabel" >Generated Code:</label>-->-->
+<!--                            <input class="form-control" type="text"  name="generatedCode" id="generatedCode" value="" readonly />-->
+<!--                        </div>-->
+                        <label class="control-label" style="display: none;width:100%" id="choose">* You must choose email or generate code</label>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" id="modelSubmit" class="btn btn-default" >Submit</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal contact Us -->
+        <div class="modal fade" id="modal_contact_us" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content" id="modal_content_contact_us">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Conduct Us </h4>
+                    </div>
+                    <div class="modal-body" id="modal_body_contact_us">
+                        <form method="post" action="#">
+                            <div class="row uniform 50%">
+                                <div class="6u 12u$(xsmall)">
+                                    <input type="text" name="name" id="name"  value="" placeholder="Name">
+                                </div>
+                                <div class="6u$ 12u$(xsmall)">
+                                    <input type="email" name="email" id="email_contact_us"  value="" placeholder="Email">
+                                </div>
+                                <div class="12u$">
+                                    <textarea name="message" id="message"  placeholder="Enter your message" rows="6"></textarea>
+                                </div>
+                                <div class="12u$">
+                                    <ul class="actions">
+                                        <li><input type="submit" value="Send Message" class="special"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 		<!-- Scripts -->
-			<script src="js/jquery.min.js"></script>
+		<script>
+			$(document).ready(function(){
+				$("#submit").click(function(){
+					if($("#getResults").val()!="") {
+						$("#div_placeCode").removeClass('has-error has-feedback');
+						$("#placeCode_required").css("display", "none");
+						$("#placeCode_ErrorStatus").css("display", "none");
+						$("#placeCode_xMark").css("display", "none");
+						alert($("#getResults").val());
+					}else {
+						$("#div_placeCode").addClass('has-error has-feedback');
+						$("#placeCode_required").css("display", "block");
+						$("#placeCode_ErrorStatus").css("display", "block");
+						$("#placeCode_xMark").css("display", "block");
+					}
+				});
+				$("#results").click(function(){
+					if($("#protein").val()!="") {
+						$("#modal_email_generateCode").modal();
+						$("#div_protein").removeClass('has-error has-feedback');
+						$("#protein_required").css("display", "none");
+						$("#protein_xMark").css("display", "none");
+						$("#protein_ErrorStatus").css("display", "none");
+					}else {
+						$("#div_protein").addClass('has-error has-feedback');
+						$("#protein_required").css("display", "block");
+						$("#protein_xMark").css("display", "block");
+						$("#protein_ErrorStatus").css("display", "block");
+					}
+				});
+				$("#modelSubmit").click(function(){
+
+					if(!$("#email").val() &&  !$("#code").is(":checked")){
+						$("#modalA_body").parents('div').addClass('has-error has-feedback');
+						$("#choose").css("display", "block");
+					}else{
+						$("#modalA_body").parents('div').removeClass('has-error has-feedback');
+						$("#choose").css("display", "none");
+						$("#modal_email_generateCode").modal('hide');
+                        if ($("#code").is(":checked")){
+                            alert ("Your code is: ")
+                        }
+					}
+				});
+			});
+		</script>
 			<script src="js/jquery.scrolly.min.js"></script>
 			<script src="js/jquery.dropotron.min.js"></script>
 			<script src="js/jquery.scrollex.min.js"></script>
--- setSelectionLists.php
+++ setSelectionLists.php
@@ -11,16 +11,16 @@ $Model->execute();
 
 $ResultModel = $Model->fetchAll(PDO::FETCH_ASSOC);
 
-
-$TrainAlgorithm = $conn->prepare("SELECT title FROM train_algorithm");
-$TrainAlgorithm->execute();
-
-$ResultTrainAlgorithm = $TrainAlgorithm->fetchAll(PDO::FETCH_ASSOC);
-
-
-$TestSet = $conn->prepare("SELECT title FROM testset");
-$TestSet->execute();
-$ResultTestSet = $TestSet->fetchAll(PDO::FETCH_ASSOC);
+//
+//$TrainAlgorithm = $conn->prepare("SELECT title FROM train_algorithm");
+//$TrainAlgorithm->execute();
+//
+//$ResultTrainAlgorithm = $TrainAlgorithm->fetchAll(PDO::FETCH_ASSOC);
+//
+//
+//$TestSet = $conn->prepare("SELECT title FROM testset");
+//$TestSet->execute();
+//$ResultTestSet = $TestSet->fetchAll(PDO::FETCH_ASSOC);
 
 
 ?>
